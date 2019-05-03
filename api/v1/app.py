8e60150b7398a232ddba8d7b3dd3caab7fc3272e
sazad44 <530@holbertonschool.com>
2019-05-03 02:08:40 +0000

    Uploading pep8 compliance change in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+
 @app.errorhandler(404)
 def not_found(invalid_path):
     """Error handling function for 404 page"""
